5cff1593cdc6aceb3c7773dbbcf18c1475cef4ec
Juanky <juancamiloherrera960@gmail.com>
2024-01-05 10:52:28 -0500

    Portafolio: favicon, Btn volver

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const btnProjects = document.querySelector("#proyectos");
 const cards = document.querySelector(".projects")
 const main = document.querySelector("#main")
-const back = document.querySelector(".back-text")
 
 btnProjects.addEventListener("click", () => {
     if (cards.classList.contains("hidden")) {
         cards.classList.toggle("hidden")
     }
-    
-    // setTimeout(() => {
-    //     main.classList.toggle("hidden")
-    // }, 1000)
 })
-
-// back.addEventListener("click", () => {
-//     main.classList.toggle("hidden")
-// })
