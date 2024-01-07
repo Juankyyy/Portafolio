8402a2421b85bc50340385aabfcfecd058560676
Juanky <juancamiloherrera960@gmail.com>
2024-01-06 21:10:31 -0500

    KFC JS: Selects

--- proyectos/KFC/js/script.js
+++ proyectos/KFC/js/script.js
@@ -1,5 +1,71 @@
 const root = document.querySelector(".root");
 
+// --- --- --- Selects --- --- ---
+const containerSelect = document.createElement("div")
+containerSelect.classList.add("container");
+root.appendChild(containerSelect);
+
+const rowSelect = document.createElement("div");
+rowSelect.className = "row mt-4";
+containerSelect.appendChild(rowSelect);
+
+// -- -- Language -- --
+const colSelect1 = document.createElement("div");
+colSelect1.className = "col-md-6 mt-3";
+rowSelect.appendChild(colSelect1);
+
+const select1 = document.createElement("select");
+select1.classList.add("form-select");
+select1.id = "language";
+colSelect1.appendChild(select1);
+
+// -- Options --
+const optionDefault1 = document.createElement("option");
+optionDefault1.setAttribute("disabled", "")
+optionDefault1.setAttribute("selected", "");
+optionDefault1.textContent = "Idioma";
+
+const optionEn = document.createElement("option");
+optionEn.setAttribute("value", "en");
+optionEn.textContent = "Inglés";
+
+const optionEs = document.createElement("option");
+optionEs.setAttribute("value", "es");
+optionEs.textContent = "Español";
+
+select1.append(optionDefault1, optionEn, optionEs);
+
+// -- -- Theme -- --
+const colSelect2 = document.createElement("div");
+colSelect2.className = "col-md-6 mt-3";
+rowSelect.appendChild(colSelect2);
+
+const select2 = document.createElement("select");
+select2.classList.add("form-select");
+select2.id = "theme";
+colSelect2.appendChild(select2);
+
+// -- Options --
+const optionDefault2 = document.createElement("option");
+optionDefault2.setAttribute("disabled", "")
+optionDefault2.setAttribute("selected", "");
+optionDefault2.textContent = "Tema";
+
+const optionLight = document.createElement("option");
+optionLight.setAttribute("value", "light");
+optionLight.textContent = "Claro";
+
+const optionDark = document.createElement("option");
+optionDark.setAttribute("value", "dark");
+optionDark.textContent = "Oscuro";
+
+const optionBlue = document.createElement("option");
+optionBlue.setAttribute("value", "blue");
+optionBlue.textContent = "Azul";
+
+select2.append(optionDefault2, optionLight, optionDark, optionBlue);
+
+// --- --- --- Cards --- --- ---
 const container = document.createElement("div");
 container.classList.add("container");
 root.appendChild(container);
@@ -43,7 +109,7 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
-// Footer
+// --- --- --- Footer --- --- ---
 const footer = document.createElement("div");
 footer.className = "mt-5 p-4 text-light bg-danger";
 root.appendChild(footer);
