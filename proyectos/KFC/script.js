4c18910be2b615091721d5f7cd857b70f60e5c64
Juanky <juancamiloherrera960@gmail.com>
2024-01-08 15:34:18 -0500

    KFC: Idioma de los Select

--- proyectos/KFC/script.js
+++ proyectos/KFC/script.js
@@ -27,14 +27,15 @@ const language = document.querySelector("#language");
 // -- Elements --
 const descriptions = document.querySelectorAll(".card-body p");
 const buttons = document.querySelectorAll(".card-body button");
-const footer = document.querySelectorAll(".footer-text *")
+const footer = document.querySelectorAll(".footer-text *");
+const languageSelect = document.querySelectorAll("#language *");
+const themeSelect = document.querySelectorAll("#theme *");
 
 // -- Text --
-const pSpanish = ["1 Sandwich BBQ Crunch (1 filete de pollo apanado)", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, pepinillos, ...", "1 Sandwich BBQ Crunch (1 filete de pollo apanado) + 1 Papa pequeña + 1 gaseosa...", "1 Kentucky Coronel hamburguesa / Sandwich (1 Filete de pechuga de pollo apanado, Ensalada...", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, pepinillos, ...", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, Ensalada, ...", "1 Sandwich Crispy BBQ (1 filete de pechuga de pollo extra grande, triple empanizado, cebolla, ...", "1 Sandwich Crispy BBQ (1 Filete de pechuga extra grande, triple empanizado, cebolla, ..."];
-const pEnglish = ["1 BBQ Crunch Sandwich (1 breaded chicken fillet)", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, pickles, ...", "1 BBQ Crunch Sandwich (1 breaded chicken fillet) + 1 small potato + 1 soda...", "1 Kentucky Colonel Burger / Sandwich (1 Breaded Chicken Breast Fillet, Salad,...", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, pickles, ...", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, Salad, ..." , "1 Crispy BBQ Sandwich (1 extra large chicken breast fillet, triple breaded, onion, ...", "1 Crispy BBQ Sandwich (1 extra large breast fillet, triple breaded, onion, ..."];
-const footerSpanish = ["Hambre de promos - KFC", "Disfruta de nuestro delicioso pollo frito, popcorn, alitas picantes, sándwiches originales, postres, y más. Conoce aquí nuestro menú KFC."];
-const footerEnglish = ["Hungry for Promos - KFC", "Enjoy our delicious fried chicken, popcorn, hot wings, original sandwiches, desserts, and more. Check out our KFC menu here."];
-
+const cardsLanguage = [["1 Sandwich BBQ Crunch (1 filete de pollo apanado)", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, pepinillos, ...", "1 Sandwich BBQ Crunch (1 filete de pollo apanado) + 1 Papa pequeña + 1 gaseosa...", "1 Kentucky Coronel hamburguesa / Sandwich (1 Filete de pechuga de pollo apanado, Ensalada...", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, pepinillos, ...", "1 Kentucky hamburguesa / Sandwich (1 filete de pechuga de pollo apanado, Ensalada, ...", "1 Sandwich Crispy BBQ (1 filete de pechuga de pollo extra grande, triple empanizado, cebolla, ...", "1 Sandwich Crispy BBQ (1 Filete de pechuga extra grande, triple empanizado, cebolla, ..."], ["1 BBQ Crunch Sandwich (1 breaded chicken fillet)", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, pickles, ...", "1 BBQ Crunch Sandwich (1 breaded chicken fillet) + 1 small potato + 1 soda...", "1 Kentucky Colonel Burger / Sandwich (1 Breaded Chicken Breast Fillet, Salad,...", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, pickles, ...", "1 Kentucky hamburger / Sandwich (1 breaded chicken breast fillet, Salad, ..." , "1 Crispy BBQ Sandwich (1 extra large chicken breast fillet, triple breaded, onion, ...", "1 Crispy BBQ Sandwich (1 extra large breast fillet, triple breaded, onion, ..."]];
+const footerLanguage = [["Hambre de promos - KFC", "Disfruta de nuestro delicioso pollo frito, popcorn, alitas picantes, sándwiches originales, postres, y más. Conoce aquí nuestro menú KFC."], ["Hungry for Promos - KFC", "Enjoy our delicious fried chicken, popcorn, hot wings, original sandwiches, desserts, and more. Check out our KFC menu here."]];
+const languagueOptions = [["Idioma", "Inglés", "Español"], ["Language", "English", "Spanish"]];
+const themeOptions = [["Tema", "Claro", "Oscuro", "Azul"], ["Theme", "Light", "Dark", "Blue"]]
 asignarIdioma();
 
 language.addEventListener("change", () => {
@@ -47,7 +48,7 @@ function asignarIdioma() {
 
     if (localStorage.getItem("language") == "es") {
         descriptions.forEach((p) => {
-            p.textContent = pSpanish[cont];
+            p.textContent = cardsLanguage[0][cont];
             cont++;
         });
 
@@ -57,12 +58,25 @@ function asignarIdioma() {
 
         cont = 0;
         footer.forEach((text) => {
-            text.textContent = footerSpanish[cont];
+            text.textContent = footerLanguage[0][cont];
             cont++;
         });
+
+        cont = 0;
+        languageSelect.forEach((option) => {
+            option.textContent = languagueOptions[0][cont];
+            cont++;
+        });
+
+        cont = 0;
+        themeSelect.forEach((option) => {
+            option.textContent = themeOptions[0][cont]
+            cont++;
+        });
+        
     } else if (localStorage.getItem("language") == "en") {
         descriptions.forEach((p) => {
-            p.textContent = pEnglish[cont];
+            p.textContent = cardsLanguage[1][cont];
             cont++;
         });
 
@@ -72,7 +86,19 @@ function asignarIdioma() {
 
         cont = 0;
         footer.forEach((text) => {
-            text.textContent = footerEnglish[cont];
+            text.textContent = footerLanguage[1][cont];
+            cont++;
+        });
+
+        cont = 0;
+        languageSelect.forEach((option) => {
+            option.textContent = languagueOptions[1][cont];
+            cont++;
+        });
+
+        cont = 0;
+        themeSelect.forEach((option) => {
+            option.textContent = themeOptions[1][cont]
             cont++;
         });
     }
