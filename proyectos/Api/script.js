c2e71e11572c358c7784a7b55eedbbf3f6d0024a
Juanky <juancamiloherrera960@gmail.com>
2024-01-11 00:48:02 -0500

    Api: Funcionalidad Eliminar

--- proyectos/Api/script.js
+++ proyectos/Api/script.js
@@ -1,43 +1,70 @@
 const resultado = fetch("https://memin.io/public/api/users")
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    data.forEach((element) => {
-        const tbody = document.querySelector("tbody");
-        const tr = document.createElement("tr");
-        tbody.appendChild(tr);
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach((element) => {
+            const tbody = document.querySelector("tbody");
+            const tr = document.createElement("tr");
+            tbody.appendChild(tr);
+
+            const id = document.createElement("td");
+            id.innerHTML = element.id;
+            tr.appendChild(id);
+
+            const name = document.createElement("td");
+            name.innerHTML = element.name;
+            tr.appendChild(name);
+
+            const email = document.createElement("td");
+            email.innerHTML = element.email;
+            tr.appendChild(email);
+
+            const acciones = document.createElement("td");
+            tr.appendChild(acciones);
+
+            // -- Editar --
+            const editar = document.createElement("a");
+            editar.setAttribute("href", "#");
+            editar.innerText = 'Editar';
+            acciones.appendChild(editar);
+
+            editar.addEventListener("click", () =>{
+                const info = (editar.parentElement).parentElement;
+                const id = info.children[0];
+                sessionStorage.setItem("id", id.innerText);
         
-        const id = document.createElement("td");
-        id.innerHTML = element.id;
-        tr.appendChild(id);
+                const name = info.children[1];
+                const email = info.children[2];
         
-        const name = document.createElement("td");
-        name.innerHTML = element.name;
-        tr.appendChild(name);
-        
-        const email = document.createElement("td");
-        email.innerHTML = element.email;
-        tr.appendChild(email);
-        
-        const acciones = document.createElement("td");
-        tr.appendChild(acciones);
-        
-        
-        const editar = document.createElement("a");
-        editar.setAttribute("href","#");
-        editar.innerText = 'Editar';
-        acciones.appendChild(editar);
-        
-        const detalles = document.createElement("a");
-        detalles.setAttribute("href","#");
-        detalles.textContent = 'Eliminar';
-        detalles.classList.add('m-2')
-        acciones.appendChild(detalles);
-        
-        const eliminar = document.createElement("a");
-        eliminar.setAttribute("href","#");
-        eliminar.textContent = 'Detalles';
-        acciones.appendChild(eliminar);
+                inputName.value =name.innerText;
+                inputEmail.value = email.innerText;
+            })
+
+            // -- Detalles --
+            const detalles = document.createElement("a");
+            detalles.setAttribute("href", "#");
+            detalles.textContent = 'Detalles';
+            detalles.classList.add('m-2')
+            acciones.appendChild(detalles);
+
+            // -- Eliminar --
+            const eliminar = document.createElement("a");
+            eliminar.setAttribute("href", "#");
+            eliminar.textContent = 'Eliminar';
+            acciones.appendChild(eliminar);
+
+            eliminar.addEventListener("click", () => {
+                const info = (eliminar.parentElement).parentElement;
+                const id = info.children[0];
+                let deleteFetch = fetch(`https://memin.io/public/api/users/${id.innerText}`, {
+                    method: "DELETE"
+                })
+                    .then(alert("Se ha eliminado correctamente"));
+            })
         })
     })
+    // .then(loading.parentNode.removeChild(loading), console.log("a"))
+    .catch((error) => {
+        console.error("Error: ", error);
+    });
